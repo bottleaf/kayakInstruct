37d069850291eea2879525362e74dfc79bc089d0
BRADLEY <bbradley@leisd.ws>
2017-09-17 15:58:42 -0500

    This is my first push

---
+++ Kayak.js
@@ -0,0 +1,197 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<meta http-equiv="expires" content="0">
+<html>
+	<head>
+		<TITLE>Kayak Instruction Inc. Launch Pad</TITLE>
+		<META NAME="Description" CONTENT="Kayak Instruction Inc. offers instruction in all white and flat water kayaking. Fleet includes Jackson, Perception, Dagger, American Canoe Association certified instructors are experts in the sport. Quality instruction at affordable prices.">
+		<META NAME="Keywords" CONTENT="kayak, kayaking, Dallas, DFW, texas, river, kayak lessons, kayak instruction, paddle, paddling, whitewater, Whitewater Kayaking, Texas whitewater, white water, sea kayak, dagger, Heritage, Jackson, kayak accessories, eskimo roll, instruction, certification, ACA, Dave Holl, news, kayak photos, kayak equipment, adventure, Summer camp, kids camp, Junior Olympics, Red River Racing Team, learn to kayak, kayaking news, New gear, used gear, paddling in texas, kayak trips, Kayak Instruction, Inc., Riot, Harmony, Mitchell, Mountain Surf, new kayak, used kayak">
+		<META HTTP-EQUIV="Window-target" CONTENT="_top">
+	</head>
+	
+	<script language="JavaScript" type="text/javascript">
+
+/*
+Cross browser Marquee script- © Dynamic Drive (www.dynamicdrive.com)
+For full source code, 100's more DHTML scripts, and Terms Of Use, visit http://www.dynamicdrive.com
+Credit MUST stay intact
+*/
+
+//Specify the marquee's width (in pixels)
+var marqueewidth="800px"
+//Specify the marquee's height
+var marqueeheight="25px"
+//Specify the marquee's marquee speed (larger is faster 1-10)
+var marqueespeed=2
+//configure background color:
+var marqueebgcolor="#FFFFFF"
+//Pause marquee onMousever (0=no. 1=yes)?
+var pauseit=1
+
+//Specify the marquee's content (don't delete <nobr> tag)
+//Keep all content on ONE line, and backslash any single quotations (ie: that\'s great):
+
+var marqueecontent='<nobr><font face="Arial"><font color=red><a href=http://www.kayakinstruct.com/kikayak.html>** Click here for our new line of KI Kayaks **  </a></font></nobr>'
+
+////NO NEED TO EDIT BELOW THIS LINE////////////
+marqueespeed=(document.all)? marqueespeed : Math.max(1, marqueespeed-1) //slow speed down by 1 for NS
+var copyspeed=marqueespeed
+var pausespeed=(pauseit==0)? copyspeed: 0
+var iedom=document.all||document.getElementById
+if (iedom)
+document.write('<span id="temp" style="visibility:hidden;position:absolute;top:-100px;left:-9000px">'+marqueecontent+'</span>')
+var actualwidth=''
+var cross_marquee, ns_marquee
+
+function populate(){
+if (iedom){
+cross_marquee=document.getElementById? document.getElementById("iemarquee") : document.all.iemarquee
+cross_marquee.style.left=parseInt(marqueewidth)+8+"px"
+cross_marquee.innerHTML=marqueecontent
+actualwidth=document.all? temp.offsetWidth : document.getElementById("temp").offsetWidth
+}
+else if (document.layers){
+ns_marquee=document.ns_marquee.document.ns_marquee2
+ns_marquee.left=parseInt(marqueewidth)+8
+ns_marquee.document.write(marqueecontent)
+ns_marquee.document.close()
+actualwidth=ns_marquee.document.width
+}
+lefttime=setInterval("scrollmarquee()",20)
+}
+window.onload=populate
+
+function scrollmarquee(){
+if (iedom){
+if (parseInt(cross_marquee.style.left)>(actualwidth*(-1)+8))
+cross_marquee.style.left=parseInt(cross_marquee.style.left)-copyspeed+"px"
+else
+cross_marquee.style.left=parseInt(marqueewidth)+8+"px"
+
+}
+else if (document.layers){
+if (ns_marquee.left>(actualwidth*(-1)+8))
+ns_marquee.left-=copyspeed
+else
+ns_marquee.left=parseInt(marqueewidth)+8
+}
+}
+
+if (iedom||document.layers){
+with (document){
+document.write('<table border="0" cellspacing="0" cellpadding="0"><td>')
+if (iedom){
+write('<div style="position:relative;width:'+marqueewidth+';height:'+marqueeheight+';overflow:hidden">')
+write('<div style="position:absolute;width:'+marqueewidth+';height:'+marqueeheight+';background-color:'+marqueebgcolor+'" onMouseover="copyspeed=pausespeed" onMouseout="copyspeed=marqueespeed">')
+write('<div id="iemarquee" style="position:absolute;left:0px;top:0px"></div>')
+write('</div></div>')
+}
+else if (document.layers){
+write('<ilayer width='+marqueewidth+' height='+marqueeheight+' name="ns_marquee" bgColor='+marqueebgcolor+'>')
+write('<layer name="ns_marquee2" left=0 top=0 onMouseover="copyspeed=pausespeed" onMouseout="copyspeed=marqueespeed"></layer>')
+write('</ilayer>')
+}
+document.write('</td></table>')
+}
+}
+</script>
+	
+
+	<STYLE>
+		<!-- 
+A{text-decoration:none} 
+--></STYLE>
+	<body>
+		<table width="100%" border="0">
+			<tr>
+				<td width="200" valign="top">
+					<script language="javascript" src="menu.js"></script>
+				</td>
+				<td width="400" valign="top">
+					<table cellspacing="0" cellpadding="0" border="0" align="center" bordercolor="#FFFFFF"
+						ID="Table1">
+						<tr bgcolor="#FFFFFF" align="center" valign="top">
+							<th>
+								<font color="#0000FF" size="4" face="Arial, Helvetica, sans-serif">Welcome To Kayak 
+									Instruction Inc.</font></th></tr>
+						<tr bgcolor="#FFFFFF" align="center" valign="top">
+							<th>
+								<img src="images/space.gif" width="9" height="11" border="0"></th></tr>
+						<tr align="center" valign="top">
+							<th>
+								<b><font size="3" face="Verdana, Arial, Helvetica, sans-serif" color="#333333">Learn it 
+										right the first time! </font></b>
+							</th>
+						</tr>
+						<tr bgcolor="#FFFFFF" align="center" valign="top">
+							<th nowrap>
+								<img src="images/space.gif" width="10" height="11" border="0"></th></tr>
+						<tr bgcolor="#FFFFFF" align="center" valign="top">
+							<th nowrap>
+								<font color="#666666"><i><font size="2" face="Arial, Helvetica, sans-serif">Get your lesson 
+											from an ACA (American Canoe Association) Certified Instructor</font></i></font><hr>
+							</th>
+						</tr>
+						<tr align="center" valign="bottom">
+							<th align="center">
+								<font color="#666666" size="2" face="Arial, Helvetica, sans-serif"><em>We are the only 
+										North Texas program with instructors certified by the ACA to teach the kayak 
+										roll and whitewater kayak programs. We also meet the ACA insurance requirements 
+										for these programs.</em></font><hr>
+							</th>
+						</tr>
+						<tr align="center" valign="middle">
+							<th height="48">
+								<p><font color="#666666" size="2" face="Arial, Helvetica, sans-serif"><em>Kayak Instruction 
+											Inc. now offers Girl Scout Safety-Wise and Boy Scout Safety Afloat Kayak 
+											Programs</em></font>
+								</p>
+							</th>
+						</tr>
+						<tr align="center" valign="middle">
+							<th height="48">
+								<p><font color="#666666" size="2" face="Arial, Helvetica, sans-serif"><em>Follow us on Facebook and Instagram for the latest updates</em></font>
+								</p>
+							</th>
+						</tr>
+						<tr align="center" valign="middle">
+							<th>
+								<a href="https://www.facebook.com/Kayak-Instruction-Inc-202657368298/" target=_blank><img src="../images/facebook.jpg" width=100></a>
+								<img src="images/KIblue_yellow.jpg" width="232" border="0" align="middle">
+								<a href="https://www.instagram.com/kayak_instruction_inc/" target=_blank><img src="../images/instagram.jpg" width=100></a>
+							</th>
+						</tr>
+						<tr><td>&nbsp;</td></tr>
+						<tr><td>&nbsp;</td></tr>
+						<tr>
+							<td align="center">
+						
+								<a href="newsletter.html"><h2><b>Newsletter</b></h2></a> <br />
+								<a href="kikayak.html"><h2><b>Store/Sales<br />Kayaks & Accessories</b></h2></a><br />
+								<!--<a href="Fleet_2011.pdf"><h2><b>Fleet Sales</b></h2></a>-->
+								<a href="foundation.html"><img src="../images/Marie1web2.jpg" width="350" /></a>
+							</td>
+						</tr>
+						<!--<tr>
+						    <td align="center">
+						    <img src="images/tshirticon.jpg" onclick=window.open("mailto:dave@kayakinstruct.com?subject=Register%20for%20January%20Tshirt%20giveaway"); />
+						    </td>
+						</tr>-->
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td align="center" width=475>
+					<!--<img src="images/blue_red_bottom.gif" width="394" height="30" border="0" align="middle">-->
+					<br><br><font face="Arial, Helvetica, sans-serif" size="3" color="red"><i><font size="2">What our lawyers make us say: Link to our <a href="KIWaiver.pdf">Insurance Waiver form</a>
+						</font></i></font><br><br>
+					<font face=ariel size=3>STOP DREAMING.............................................START PADDLING</font><br />
+					<font color=blue>All Rights Reserved <font face=helvetica size=3><strong>KAYAK</strong></font> Instruction Inc. 2016</font>
+					<p align="center"><em><font color="#666666" size="1">Participants will not use 
+								alcohol&nbsp;and other substances that may impair judgment and reduce a 
+								participant's ability to effectively manage the risks of water travel</font></em></p>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
